98fbeab61e77ba19e25e10e02984a4ccc1809407
Guro Bokum <jiojiajiu@gmail.com>
2016-01-09 07:17:46 +0300

    new server routes

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express();
 
-app.use('/', express.static('../build'));
 app.use('/templates', express.static('../src/templates'));
+app.use('/directives', express.static('../src/directives'));
+app.use('/components', express.static('../src/templates'));
+app.use('/data', express.static('../src/data'));
+app.use('/', express.static('../build'));
 
 app.listen(8000);
 console.log('Started :8000')
